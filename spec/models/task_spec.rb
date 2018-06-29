94cb3568a0b0252cd09e1157af31d7b6154d0342
DC <dachang.h@gmail.com>
2018-06-29 20:39:33 +0800

    步驟12: 資料驗證

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '步驟12' do
+    # it '資料驗證' do
+    #   task_size = Task.all.size
+    #   Task.create(title: 'aa', description: '')
+    #   Task.create(title: '', description: 'bb')
+    #   expect(Task.all.size).to eq task_size
+    # end
+
+    it '資料驗證 - 標題必填' do
+      task = Task.create(title: '', description: 'xx')
+      expect(task).to_not be_valid
+      expect(task.errors.messages[:title].first).to_not be nil?
+    end
+
+    it '資料驗證 - 內容必填' do
+      task = Task.create(title: 'xx', description: '')
+      expect(task).to_not be_valid
+      expect(task.errors.messages[:description].first).to_not be nil?
+    end
+
+  end
+
 end
