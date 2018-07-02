e26ee4b759b7123b518e7e6cd4b96bdf1aae8fd4
DC <dachang.h@gmail.com>
2018-07-02 22:06:13 +0800

    步驟17: 增加分頁功能

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -7,18 +7,17 @@ RSpec.describe 'tasks/index.html.erb' do
     @new_task = Task.create(title: 'new_title', description: 'new_desc', end_at: Time.now + 1.day, status: 1, priority: 1)
   end
 
-  # 兩排序規則需要同時存在？
-  # describe '步驟11' do
-  #   it '任務列表以建立時間排序' do
-  #    visit '/tasks'
-  #    page.body.index(@new_task.title).should < page.body.index(@first_task.title)
-  #   end
-  # end
+  describe '步驟11' do
+    it '任務列表以建立時間排序' do
+     visit '/tasks'
+     expect(page.body.index(@new_task.title)).to be > page.body.index(@first_task.title)
+    end
+  end
 
   describe '步驟14' do
     it '列表頁能夠以結束時間排序(離結束時間越近排序越前)' do
       visit '/tasks'
-      page.body.index(@new_task.title).should > page.body.index(@first_task.title)
+      expect(page.body.index(@new_task.title)).to be > page.body.index(@first_task.title)
     end
   end
 
